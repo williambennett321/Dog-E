0d3b0cce7ba6fd2397cfc6ed8edf75c763248f3c
williambennett321 <william_bennett@DESKTOP-U4DJP4P.localdomain>
2021-11-08 17:34:29 -0500

    add new page to add dog

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -10,14 +10,21 @@ function index(req,res) {
   })
 }
 
-function show(req,res) {
-  Profile.findById(req.user.profile)
-  .then(userProfile => { 
-res.render("profiles/show" , {
-  title: `Profile : ${profile.name}`,
-  profile,
-  userProfile
+function show(req, res) {
+  Profile.findById(req.params.id)
+  .then((profile) => {
+    Profile.findById(req.user.profile)
+    .then(userProfile => {
+      res.render("profiles/show", {
+        title: `${profile.name}'s profile`,
+        profile,
+        userProfile,
+      })
     })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/")
   })
 }
 
